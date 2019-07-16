9a0a6be43093e1b2b2a5e4d67a012997b54af04a
IronChang <408662026@qq.com>
2019-07-16 16:35:01 +0800

    利用快慢指针判断链表是否有环

---
+++ 判断环形链表.c
@@ -0,0 +1,26 @@
+
+  Definition for singly-linked list.
+struct ListNode 
+{
+      int val;
+     struct ListNode *next;
+};
+ 
+ //快慢指针解决问题 
+bool hasCycle(struct ListNode *head)
+{
+    struct ListNode *slow = head;
+	struct ListNode *fast = head;
+	
+	while(fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		
+		if(slow == fast)
+		{
+			return true;
+		}
+	 } 
+	 return false; 
+}
