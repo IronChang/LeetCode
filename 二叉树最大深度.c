2d8144672fee158acb2f80c750107f650430bbfa
IronChang <408662026@qq.com>
2019-07-23 11:23:38 +0800

    利用递归求解二叉树最大深度

---
+++ 二叉树最大深度.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int maxDepth(struct TreeNode* root){
+  if(root == NULL)
+      return 0;
+    
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return left>right?left+1:right+1;
+}
+
+
