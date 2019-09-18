2bfdbf893b0001427ddf606be66ede20b9360440
IronChang <408662026@qq.com>
2019-09-18 18:10:18 +0800

    利用string库函数去反转一个字符串

---
+++ 反转字符串(string).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) 
+    {
+        int begin = 0;
+        int end = s.size()-1;
+        
+        while(begin<end)
+        {
+            swap(s[begin],s[end]);
+            ++begin;
+            --end;
+        }
+    }
+};
