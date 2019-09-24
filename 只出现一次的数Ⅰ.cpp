331b6044202e4b232e315b215e7b22733be3bedf
IronChang <408662026@qq.com>
2019-09-24 22:17:16 +0800

    利用异或求解 + vector性质

---
+++ 只出现一次的数Ⅰ.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int val = 0;
+        for(int i = 0;i<nums.size();++i)
+        {
+            val ^= nums[i];
+        }
+        
+        return val;
+    }
+};
