83d79f35ca4b2432dc65ce4a54bf6f7c4788d970
IronChang <408662026@qq.com>
2019-12-01 21:48:17 +0800

    一组数据中出现次数超过数组长度一般的数字

---
+++ 一组数据中出现次数超过数组长度一般的数字.cpp
@@ -0,0 +1,27 @@
+//一组数据中出现次数超过数组长度一般的数字
+
+//class Gift {
+//public:
+//	int getValue(vector<int> gifts, int n) {
+//		sort(gifts.begin(), gifts.end());
+//		int hs = n / 2;
+//		for (int i = 0; i + hs < n; i++)
+//		{
+//			if (gifts[i] == gifts[i + hs])
+//				return gifts[i];
+//		}
+//		return 0;
+//	}
+//};
+//
+//int main()
+//{
+//	Gift s;
+//	int a[] = { 1, 2, 3, 2, 2 };
+//	vector<int> v(a, a + (sizeof(a) / sizeof(a[0])));
+//	cout << s.getValue(v, (sizeof(a) / sizeof(a[0]))) << endl;
+//	system("pause");
+//	return 0;
+//}
+//
+
